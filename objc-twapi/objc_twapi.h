b6645991c99b58f6ac31dcede703894510a7ed2b
Or Sagi <orsagi@gmail.com>
2012-12-25 00:29:36 +0200

    Initial Commit

---
+++ objc-twapi/objc_twapi.h
@@ -0,0 +1,13 @@
+//
+//  objc_twapi.h
+//  objc-twapi
+//
+//  Created by Or Sagi on 25/12/12.
+//  Copyright (c) 2012 translatewiki.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface objc_twapi : NSObject
+
+@end
