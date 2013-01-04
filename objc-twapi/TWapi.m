86f2bb087b403403d32bbc0cb5f943c06924a3be
Or Sagi <orsagi@gmail.com>
2013-01-04 18:50:28 +0200

    encoded url query support
    plus little bit more in-code documentation

--- objc-twapi/TWapi.m
+++ objc-twapi/TWapi.m
@@ -8,11 +8,13 @@
 
 #import "TWapi.h"
 
+#define SERV_PATH @"https://translatewiki.net/w/api.php"
+
 @implementation TWapi
 
 +(NSDictionary *)TWRequest:(NSDictionary *)params
 {
-    NSString *URLPath = @"https://translatewiki.net/w/api.php";
+    NSString *URLPath = SERV_PATH;
     NSString *URLComplete = @"";
     NSMutableDictionary *requestParams = [NSMutableDictionary alloc];
     requestParams = [requestParams initWithDictionary:params];
@@ -20,17 +22,19 @@
     URLComplete = [URLComplete stringByAppendingString:URLPath];
     URLComplete = [URLComplete stringByAppendingString:@"?"];
     
+    //build URL line with query parameters - taken from requestParams dictionary.
     for( NSString *aKey in requestParams )
     {
-        //TODO: add encoding check
         URLComplete = [URLComplete stringByAppendingString:aKey];
         URLComplete = [URLComplete stringByAppendingString:@"="];
-        URLComplete = [URLComplete stringByAppendingString:[requestParams valueForKey:aKey]];
+        URLComplete = [URLComplete stringByAppendingString:[[requestParams valueForKey:aKey] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
         URLComplete = [URLComplete stringByAppendingString:@"&"];
     };
-    URLComplete = [URLComplete stringByPaddingToLength:[URLComplete length]-1 withString:0 startingAtIndex:0];
+    URLComplete = [URLComplete stringByPaddingToLength:[URLComplete length]-1 withString:0 startingAtIndex:0]; //ommit last '&'
     
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:URLComplete]];
+    NSString * escapedURLComplete = [URLComplete stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding]; //url encoding
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:escapedURLComplete]];
     
     [request setHTTPMethod:@"POST"];
     
@@ -40,7 +44,7 @@
     NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
     
     if([responseCode statusCode] != 200){
-        NSLog(@"Error getting %@, HTTP status code %i", URLComplete, [responseCode statusCode]);
+        NSLog(@"Error getting %@, HTTP status code %i", escapedURLComplete, [responseCode statusCode]);
         return 0;
     }
     
@@ -52,7 +56,7 @@
 }
 
 
-+(NSDictionary *)TWQueryRequest:(NSDictionary *)params
++(NSDictionary *)TWQueryRequest:(NSDictionary *)params //general query - additional wrap is needed
 {
     NSMutableDictionary *requestParams = [NSMutableDictionary alloc];
     requestParams = [requestParams initWithDictionary:params];
@@ -60,28 +64,30 @@
     return [self TWRequest:requestParams];
 }
 
+
 +(NSString *)TWLoginRequestForUser:(NSString*)username WithPassword:(NSString*) passw
 {
     NSMutableDictionary *requestParams = [[NSMutableDictionary alloc] init];
+    //add the login parameters
     [requestParams setObject:username forKey:@"lgname"];
     [requestParams setObject:passw forKey:@"lgpassword"];
     [requestParams setObject:@"login" forKey:@"action"];
+    
     NSDictionary * responseData;
-    responseData =  [TWapi TWRequest:requestParams];
+    responseData =  [TWapi TWRequest:requestParams]; //get response
     
     id lgid = [responseData objectForKey:@"login"];
-    
     NSString *result = [[NSString alloc] initWithFormat:@"%@",[lgid valueForKey:@"result"]];
     
-    if ([result isEqualToString:@"NeedToken"])
+    if ([result isEqualToString:@"NeedToken"])  //now we have a first response, we need to make a second request with a 'token'
     {
-        [requestParams setObject:[lgid valueForKey:@"token"] forKey:@"lgtoken"];
-        responseData =  [TWapi TWRequest:requestParams];
+        [requestParams setObject:[lgid valueForKey:@"token"] forKey:@"lgtoken"]; //add the token parameter
+        responseData =  [TWapi TWRequest:requestParams];  //second request
         lgid = [responseData objectForKey:@"login"];
         result = [lgid valueForKey:@"result"];
     }
     
-    return (result);
+    return (result); //return the login result [Success, NotExists, WrongPass...]
 }
 
 +(NSDictionary *)TWLogoutRequest:(NSDictionary *)params
@@ -100,6 +106,6 @@
     return [self TWRequest:requestParams];
 }
 
-
+//TODO add some more wrapper functionalities...
 
 @end
