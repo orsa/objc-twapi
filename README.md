0ce7d60e20a79ad0f14e4aecf19601df3f3b35ba
Or Sagi <orsagi@gmail.com>
2013-04-25 17:16:28 +0300

    liscense declaration

--- README.md
+++ README.md
@@ -9,6 +9,20 @@ A listing of the API wrapper methods and their contract/decription will be added
 
 please give us your feedback.
 
+Copyright 2013 Or Sagi, Tomer Tuchner
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 ------------------
 Usage examples :
 
@@ -31,3 +45,5 @@ Usage examples :
     responseData = [TWapi TWQueryRequest:parameters]; //call a query request via API wrapper
 
 // for a detailed API specification rerefer to: https://translatewiki.net/w/api.php
+
+
--- objc-twapi/TWapi.m
+++ objc-twapi/TWapi.m
@@ -3,10 +3,23 @@
 //  TWapi
 //
 //  Created by Or Sagi on 21/12/12 (end of the world).
-//  Copyright (c) 2012 Or Sagi. All rights reserved.
+//
+//  Copyright 2013 Or Sagi, Tomer Tuchner
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 //*********************************************************************************
-// This is an API wrapper implementation for: https://translatewiki.net/w/api.php .
+// This is an API wrapper implementation for: https://translatewiki.net/w/api.php
 // Refer to: https://github.com/orsa/objc-twapi .
 //*********************************************************************************
 //
